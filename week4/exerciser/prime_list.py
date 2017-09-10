0464c386d0047acffaa1ff33ab99040ce7811518
xiaoyuhen <xiaoyuhen1994@gmail.com>
2017-09-10 21:03:35 +0800

    add week4 exerciser

---
+++ week4/exerciser/prime_list.py
@@ -0,0 +1,14 @@
+# Prime number lists
+
+###################################################
+# Student should enter code below
+
+print_lists = [2, 3, 5, 7, 11, 13]
+print print_lists[1], print_lists[3], print_lists[5]
+
+###################################################
+# Expected output
+
+#3 7 13
+
+
---
+++ week4/exerciser/vector_addition.py
@@ -0,0 +1,27 @@
+# Vector addition function
+
+###################################################
+# Student should enter code below
+
+def add_vector(a, b):
+    x = [(a[0] + b[0]), (a[1] + b[1])]
+    return x
+
+
+###################################################
+# Test
+
+print add_vector([4, 3], [0, 0])
+print add_vector([1, 2], [3, 4])
+print add_vector([2, 3], [-6, -3])
+
+
+
+###################################################
+# Output
+
+#[4, 3]
+#[4, 6]
+#[-4, 0]
+
+
